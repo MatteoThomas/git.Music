80a82a0c54657cdb9820edcf5a0780353fad62db
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-12-12 14:05:05 -0700

    updated readme, del DS_Store

--- README.md
+++ README.md
@@ -1,63 +1,63 @@
-# Git-Music-Now-Project
-Project 1 bootcamp
+# gitMusic
 
+
+![Homepage screenshot](/gitmusic.png)
 
 [![forthebadge](https://forthebadge.com/images/badges/uses-html.svg)](https://forthebadge.com)
-
 [![forthebadge](https://forthebadge.com/images/badges/uses-css.svg)](https://forthebadge.com)
-
 [![forthebadge](https://forthebadge.com/images/badges/uses-js.svg)](https://forthebadge.com)
 
-
 ## Table of Contents
-1. [Title](#title)
-2. [General Info](#general-info)
-3. [Technologies](#technologies)
-4. [Installation](#installation)
-5. [Collaboration](#collaboration)
-6. [Steps](#Steps)
 
+-[Description](#description)
 
+-[Technologies](#technologies)
 
-## Title
-Git Music Now!
+-[Installation](#installation)
 
-## General Info
-The purpose of this app is to create a music application that allows users to search by artist and be able to listen to sample music from the artist’s album as well as be provided with a list of songs by the artist. 
+-[Contributors](#collaboration)
 
-## Technologies 
-The site uses HTML, CSS and Javascript
+-[Steps](#steps)
+
+## Description
+
+The purpose of this app is to create a music application that allows users to search by artist and be able to listen to sample music from the artist’s album as well as be provided with a list of songs by the artist.
+
+## Technologies
+
+Express
+
+APIs
 
 ## Installation
-To access the website go to https://rlacer.github.io/Git-Music-Now-Project/
 
+npm start
 
+## Contributors
 
-# Collaboration 
-## Contributors 
-* @MelElson 
-* @RLacer
-* @MatteoThomas
-* @CoderLeE920
-
+- @MelElson
+- @RLacer
+- @MatteoThomas
+- @CoderLeE920
 
 # Steps
+
 Steps taken by user
-* When the user inputs artist name they are given an image of the artist/band. 
-* The user is given music sample of the artist that they can listen to with built in player. 
-* The user is also presented with a list of songs from this artist.
-* When the user searches another artist the current image and information is removed and the user is presented with the image and selection for new artist. 
-* When the user refreshes the page the previous search is shown
-	
-  
+
+- When the user inputs artist name they are given an image of the artist/band.
+- The user is given music sample of the artist that they can listen to with built in player.
+- The user is also presented with a list of songs from this artist.
+- When the user searches another artist the current image and information is removed and the user is presented with the image and selection for new artist.
+- When the user refreshes the page the previous search is shown
+
 ### Git Music
+
 ![Changed HTML](./img/screenshotgitmusic.png)
 
 ### Search
+
 ![Changed HTML](./img/screenshotsearch.png)
 
 ### List
+
 ![Changed HTML](./img/screenshotlist.png)
-
-
-
--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var requirejs = require("requirejs");
+require("dotenv").config();
+
 var searchButton = document.getElementById("search-button");
 var player = document.getElementById("player");
 var title = document.getElementById("artist");
@@ -5,135 +8,120 @@ var artistName = document.getElementsByClassName("artist");
 var header = document.getElementsByClassName("header");
 var song = document.getElementById("songs");
 var album = document.getElementById("albums");
-var more = document.getElementById('more');
+var more = document.getElementById("more");
 $("#song_album").hide();
 $("#more").hide();
 $("#player_container").hide();
 
-
 function getAPI() {
+  $("#more").show();
+  $("#song_album").show();
+  $("#player_container").show();
+  $("#page-info").hide();
 
-    $("#more").show();
-    $("#song_album").show();
-    $("#player_container").show();
-    $("#page-info").hide();
+  var artist = document.getElementById("input").value;
 
-    var artist = document.getElementById("input").value;
+  $(artistName).html("");
+  $(artistName).html(artist);
 
-    $(artistName).html("");
-    $(artistName).html(artist);
-
-
-    fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-key": "69b90c75b0msh9037becee190a85p1d61ccjsn154266cd5838",
-            "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com"
-        }
+  fetch(`https://deezerdevs-deezer.p.rapidapi.com/search?q=${artist}`, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-key": "69b90c75b0msh9037becee190a85p1d61ccjsn154266cd5838",
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-        .then(function (response) {
-            return response.json();
-          
-        })
-        .then(function (data) {
-            $("#more").html("");
-            console.log(data);
-            console.log(data.data[0].preview);
-            console.log(data.data[0].title);
-            console.log(data.data[0].album.title);
-            song = data.data[0].title;
-            album = data.data[0].album.title;
-            $('#songs').html("");
-            $('#songs').append(song);
-            $('#albums').html("");
-            $('#albums').html(album);
+    .then(function (data) {
+      $("#more").html("");
+      console.log(data);
+      console.log(data.data[0].preview);
+      console.log(data.data[0].title);
+      console.log(data.data[0].album.title);
+      song = data.data[0].title;
+      album = data.data[0].album.title;
+      $("#songs").html("");
+      $("#songs").append(song);
+      $("#albums").html("");
+      $("#albums").html(album);
 
+      for (var i = 1; i <= 8; i++) {
+        var songsEl = document.createElement("li");
+        songsEl.innerHTML = `<a class="song_link" value=${data.data[i].preview}>${data.data[i].title} from ${data.data[i].album.title}</a>`;
 
-            for (var i = 1; i <= 8; i++) {
+        $("#more").append(songsEl);
+      }
 
-                var songsEl = document.createElement("li");
-            songsEl.innerHTML = `<a class="song_link" value=${data.data[i].preview}>${data.data[i].title} from ${data.data[i].album.title}</a>`;
-            
-            $("#more").append(songsEl);
-            }
-
-
-            $(".song_link").click(function () {
-                var url = $(this).attr("value");
-                var details = $(this).text();
-                var detail_array = details.split(" from ");
-                $("#songs").text(detail_array[0]);
-                $("#albums").text(detail_array[1]);
-                album = detail_array[1];
-                console.log(detail_array[0]);
-                console.log(url);
-                player.setAttribute("src", url);
-                console.log($(this).val());
-                
-            })
-            player.setAttribute("src", `${data.data[0].preview}`);
-            
-
-
-        })
+      $(".song_link").click(function () {
+        var url = $(this).attr("value");
+        var details = $(this).text();
+        var detail_array = details.split(" from ");
+        $("#songs").text(detail_array[0]);
+        $("#albums").text(detail_array[1]);
+        album = detail_array[1];
+        console.log(detail_array[0]);
+        console.log(url);
+        player.setAttribute("src", url);
+        console.log($(this).val());
+      });
+      player.setAttribute("src", `${data.data[0].preview}`);
+    });
 }
 
 // discogs API call
 function discAPI() {
-    let url = "https://api.discogs.com/database/search?q="
-    let artist = $('#input').val();
-    let apiKey = "&key=DspsPlrDDgNBHyZQSnHV";
-    let secret = "&secret=JtsCNMKigmGKAhrugoBTVSyTLESOZUZT"
-    let total = url + artist + apiKey + secret;
-    //  $('#artist-name').html("");
-    $('#artist-name').append(artist);
-    $('.input').val("");
+  let url = "https://api.discogs.com/database/search?q=";
+  let artist = $("#input").val();
+  let apiKey = (myAPIKey = process.env.DISCOGS_API_KEY);
+  let secret = (myAPISecret = process.env.DISCOGS_SECRET);
+  let total = url + artist + apiKey + secret;
+  //  $('#artist-name').html("");
+  $("#artist-name").append(artist);
+  $(".input").val("");
 
-    // fetched data from api
-    fetch(total).then(function (response) {
-        return response.json();
-    }).then(function (data) {
-
-        // gets pic from array
-        var pic = data.results[0].cover_image
-        console.log(data)
-        var img = $('<img class="box" src=' + pic + '>', {});
- 
-        // adds image from discogs to the page
-
-        $('#img').append(img);
-        https: //www.discogs.com/search/?q=korn&type=all
-
-
-            localStorage.setItem('artStore', artist);
-        // Retrieve
-
-        var item = localStorage.getItem('artStore');
-        console.log(item)
+  // fetched data from api
+  fetch(total)
+    .then(function (response) {
+      return response.json();
     })
-    $("#list-group").show();
+    .then(function (data) {
+      // gets pic from array
+      var pic = data.results[0].cover_image;
+      console.log(data);
+      var img = $('<img class="box" src=' + pic + ">", {});
+
+      // adds image from discogs to the page
+
+      $("#img").append(img);
+      //www.discogs.com/search/?q=korn&type=all
+
+      https: localStorage.setItem("artStore", artist);
+      // Retrieve
+
+      var item = localStorage.getItem("artStore");
+      console.log(item);
+    });
+  $("#list-group").show();
 }
 
-
-
-
-setStore()
+setStore();
 
 function setStore() {
-    var item = localStorage.getItem('artStore');
-    console.log(item)
+  var item = localStorage.getItem("artStore");
+  console.log(item);
 
-    $('#list-group').append(item);
-
+  $("#list-group").append(item);
 }
 
 // removes pevious searches image
 function removeImage() {
-    $("#img").html("");
+  $("#img").html("");
 }
 
 function showMore() {
-    $("#more").show();
+  $("#more").show();
 }
 
 // event listeners
@@ -141,11 +129,11 @@ searchButton.addEventListener("click", getAPI);
 searchButton.addEventListener("click", discAPI);
 
 // event listener allows enter key to trigger APIs, clear search field, clear image
-$('.input').on('keypress', function (e) {
-    if (e.which == 13) {
-        console.log("enter key pressed")
-        getAPI()
-        discAPI()
-        removeImage()
-    }
+$(".input").on("keypress", function (e) {
+  if (e.which == 13) {
+    console.log("enter key pressed");
+    getAPI();
+    discAPI();
+    removeImage();
+  }
 });
